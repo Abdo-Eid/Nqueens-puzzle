6eedc6d0a1acb94a38346b5643c9aa1b4087f1bf
Abdo-Eid <65284648+Abdo-Eid@users.noreply.github.com>
2023-12-31 16:07:46 +0200

    some comments

--- src/hill_climbingS.py
+++ src/hill_climbingS.py
@@ -16,14 +16,15 @@ def hill_climbing(board, max_iterations=50050,non_improving_limit = 10040):
         # another approtch is to get random queen change the pos to random col
         # if there improve select that state
         for _ in range(n^2):
-            queen = randint(0,n-1)
-            col = randint(0,n-1)
+            queen = randint(0,n-1) # index of the row
+            col = randint(0,n-1) 
+            # if the new queen not the same as the current then make the new state
             if current_state.pos[queen] != col:
 
                 neighbor_state = current_state.copy()
                 neighbor_state.changePos(queen,col)
                 neighbor_conflicts = neighbor_state.conflicts()
-
+                # if the number of conflicts of the new state is better take it
                 if neighbor_conflicts <= current_conflicts:
 
                     current_state = neighbor_state
